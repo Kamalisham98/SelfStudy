bd256d8586335648e95faff5a31dae4d86144043
kamal isham <kamalisham98@gmail.com>
2021-03-25 10:57:47 +0700

    update day 19 mysql

--- Week 4/Day 19/controller/transaksiController.js
+++ Week 4/Day 19/controller/transaksiController.js
@@ -1,57 +1,117 @@
 // Import mysql connection
-const connection = require("../models");
 const mysql = require("../models");
 
 //get All data from transaksi
-const getAll = (req,res)=>{
-    let sql = "SELECT * FROM transaksi t JOIN barang b on t.id_barang = b.id JOIN pelanggan p ON t.id_pelanggan =p.id";
-    mysql.query(sql,(err,result)=>{
-        if (err){
-            return res.status(500).json({
-                message: "Internal Sever Error",
-                error:err,
-            });
-        }
-        return res.status(200).json({
-            message:"Success",
-            data: result,
-        });
+const getAll = (req, res) => {
+  let sql =
+    "SELECT t.id, p.nama as nama_pelanggan, b.nama as nama_barang, b.harga, pem.nama as nama_pemasok, t.waktu, t.jumlah, t.total FROM transaksi t JOIN barang b ON t.id_barang = b.id JOIN pelanggan p ON p.id = t.id_pelanggan JOIN pemasok pem ON b.id_pemasok = pem.id";
+  mysql.query(sql, (err, result) => {
+    if (err) {
+      return res.status(500).json({
+        message: "Internal Sever Error",
+        error: err,
+      });
+    }
+    return res.status(200).json({
+      message: "Success",
+      data: result,
     });
-}
+  });
+};
 
 const create = (req, res) => {
-    // Find Price of barang
-    let sqlFindBarang = 'SELECT * FROM barang WHERE id = ?';
+  // Find Price of barang
+  let sqlFindBarang = "SELECT * FROM barang WHERE id = ?";
 
-    //Run sqlFindBarang 
-    mysql.query(sqlFindBarang, [req.body.id_barang], (err,result)=>{
-        let price = eval(result[0].harga);
-        let total = eval(req.body.jumlah * price);
+  //Run sqlFindBarang
+  mysql.query(sqlFindBarang, [req.body.id_barang], (err, result) => {
+    let price = eval(result[0].harga);
+    let total = eval(req.body.jumlah * price);
 
-        let sqlCreate = 
-        "INSERT INTO transaksi(id_barang, id_pelanggan, jumlah, total) VALUES (?,?,?,?)";
+    let sqlCreate =
+      "INSERT INTO transaksi(id_barang, id_pelanggan, jumlah, total) VALUES (?,?,?,?)";
 
-        mysql.query(sqlCreate,
-            [req.body.id_barang, req.body.id_pelanggan, req.body.jumlah, total],
-            (err,result) =>{
-                if(err){
-                return res.status(500).json({
-                    message:"Internal Sever Error",
-                    error: err,
-                });
-                }
-                return res.status(200).json({
-                    message:"Success",
-                    data: result,
-                });
-            }
-        );
-        
+    mysql.query(
+      sqlCreate,
+      [req.body.id_barang, req.body.id_pelanggan, req.body.jumlah, total],
+      (err, result) => {
+        // if error
+        if (err) {
+          return res.status(500).json({
+            message: "Internal Sever Error",
+            error: err,
+          });
+        }
+        //  if success
+        let sqlSelect = `SELECT * FROM transaksi t JOIN barang b on t.id_barang = b.id JOIN pelanggan p ON t.id_pelanggan =p.id  WHERE t.id = ${result.insertId}`;
+
+        mysql.query(sqlSelect, (err, result) => {
+          // if Error
+          if (err) {
+            return res.status(500).json({
+              message: "Internal Sever Error",
+              error: err,
+            });
+          }
+          return res.status(200).json({
+            message: "Success",
+            data: result[0],
+          });
+        });
+      }
+    );
+  });
+};
+
+const deleteData = (req, res) => {
+  let sqlDelete = "DELETE FROM transaksi WHERE id = ?";
+
+  mysql.query(sqlDelete, [req.params.id], (err, result) => {
+    // if error
+    if (err) {
+      return res.status(500).json({
+        message: "Internal Server Error",
+        error: err,
+      });
+    }
+    // if success
+    return res.status(200).json({
+      message: "Success",
     });
+  });
+};
 
+const update = (req, res) => {
+  // Find Barang
+  let sqlFindBarang = "SELECT * FROM barang WHERE id = ? ";
+  // Run Find query
+  mysql.query(sqlFindBarang, [req.body.id_barang], (err, result) => {
+    let price = eval(result[0].harga);
+    let total = eval(req.body.id_barang * price);
 
+    //Declare update
+    let sqlUpdate =
+      "Update transaksi SET id_barang = ?, jumlah = ?, total = ? WHERE id = ?";
 
-}
+    mysql.query(
+      sqlUpdate,
+      [req.body.id_barang, req.body.jumlah, total, req.params.id],
+      (err, result) => {
+        //if error
+        if (err) {
+          return res.status(500).json({
+            message: "Internal Server Error",
+            error: err,
+          });
+        }
+        //if success
+        return res.status(200).json({
+          message: "Success",
+          data: result,
+        });
+      }
+    );
+  });
+};
 
-
-module.exports =  {getAll, create}; // Export getAll function
+module.exports = { getAll, create, deleteData, update }; // Export getAll function
--- Week 4/Day 19/routes/transaksiRoutes.js
+++ Week 4/Day 19/routes/transaksiRoutes.js
@@ -6,5 +6,7 @@ const transaksiController = require("../controller/transaksiController");
 // Define routes
 router.get("/", transaksiController.getAll);
 router.post("/", transaksiController.create);
+router.delete("/:id", transaksiController.deleteData);
+router.put("/:id", transaksiController.update);
 
 module.exports = router; // export router
